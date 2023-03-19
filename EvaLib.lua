1271c017e9dcdbac4a7c52213cd95c42ab1f4fd4
EvaCord <112832151+PivoSteve@users.noreply.github.com>
2023-03-19 20:11:55 +0600

    ※ added EvaLib

---
+++ EvaLib.lua
@@ -0,0 +1,18 @@
+--[[
+▄▄▄ . ▌ ▐· ▄▄▄· ▄▄▌  ▪  ▄▄▄▄· 
+▀▄.▀·▪█·█▌▐█ ▀█ ██•  ██ ▐█ ▀█▪
+▐▀▀▪▄▐█▐█•▄█▀▀█ ██▪  ▐█·▐█▀▀▀▄
+▐█▄▄▌ ███ ▐█ ▪▐▌▐█▌▐▌▐█▌██▄▪▐█
+ ▀▀▀ . ▀   ▀  ▀ .▀▀▀ ▀▀▀·▀▀▀▀ 
+
+Cleverly done, Mr. Freeman, but you're not supposed to be here. 
+As a matter of fact, you're not. 
+Get back where you belong, and forget about all this, until we meet again.
+
+https://static.wikia.nocookie.net/halflife/images/c/c9/Gman_12.ogg/revision/latest?cb=20210516190003&path-prefix=ru
+]]--
+--version
+beta_next_update_announcement = "no date announced"
+prem_next_update_announcement = "no date announced"
+free_next_update_announcement = "no date announced"
+--version end
