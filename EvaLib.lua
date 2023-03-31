a161aa89f7c178887b08d4e04e4574458e1c044b
EvaCord <112832151+PivoSteve@users.noreply.github.com>
2023-03-31 21:29:05 +0600

    Updated

--- EvaLib.lua
+++ EvaLib.lua
@@ -10,9 +10,6 @@ Get back where you belong, and forget about all this, until we meet again.
 https://static.wikia.nocookie.net/halflife/images/c/c9/Gman_12.ogg/revision/latest?cb=20210516190003&path-prefix=ru
 ]]--
 
-beta_next_update_announcement = "31.03.23"
-prem_next_update_announcement = "08.04.23"
-
 evalib = {
     next_update_announcement = {
         beta = "no date announced",
