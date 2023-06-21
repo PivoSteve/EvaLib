666152e4d25441b55b842cb17e76297faa2ce1a8
EvaCord <112832151+PivoSteve@users.noreply.github.com>
2023-06-21 21:26:12 +0600

    Update EvaLib.lua

--- EvaLib.lua
+++ EvaLib.lua
@@ -12,25 +12,25 @@ https://static.wikia.nocookie.net/halflife/images/c/c9/Gman_12.ogg/revision/late
 
 evalib = {
     next_update_announcement = {
-        beta = "no date",
-        prem = "19.06.23",
-        free = "no date",
+        beta = "no date announced",
+        prem = "22.06.23",
+        free = "no date announced",
     },
 
     last_update = {
-        beta = "17.06.23",
+        beta = "21.06.23",
         prem = "06.06.23",
         free = "08.05.23",
     },
     
     current_version = {
-        beta = "10",
+        beta = "10.5",
         prem = "9",
         free = "8.1",
     },
  
     key_check = {
-        beta = "DqzGXu",
+        beta = "ccHDR0",
         prem = "wXKFNj",
     },
 
