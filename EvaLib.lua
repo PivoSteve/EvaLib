69429781cb3e7709a047bf27a4da21917932a424
EvaCord <112832151+PivoSteve@users.noreply.github.com>
2023-06-22 18:11:54 +0600

    Update EvaLib.lua

--- EvaLib.lua
+++ EvaLib.lua
@@ -1,4 +1,6 @@
 --[[
+
+
 ▄▄▄ . ▌ ▐· ▄▄▄· ▄▄▌  ▪  ▄▄▄▄· 
 ▀▄.▀·▪█·█▌▐█ ▀█ ██•  ██ ▐█ ▀█▪
 ▐▀▀▪▄▐█▐█•▄█▀▀█ ██▪  ▐█·▐█▀▀▀▄
@@ -8,35 +10,37 @@ Cleverly done, Mr. Freeman, but you're not supposed to be here.
 As a matter of fact, you're not. 
 Get back where you belong, and forget about all this, until we meet again.
 https://static.wikia.nocookie.net/halflife/images/c/c9/Gman_12.ogg/revision/latest?cb=20210516190003&path-prefix=ru
+
+
 ]]--
 
 evalib = {
     next_update_announcement = {
         beta = "no date announced",
-        prem = "22.06.23",
+        prem = "no date announced",
         free = "no date announced",
-    },
+    }, --no date announced
 
     last_update = {
         beta = "21.06.23",
-        prem = "06.06.23",
+        prem = "22.06.23",
         free = "08.05.23",
-    },
+    }, --no date
     
     current_version = {
         beta = "10.5",
-        prem = "9",
+        prem = "10.5",
         free = "8.1",
-    },
+    }, --0.0
  
     key_check = {
         beta = "ccHDR0",
-        prem = "wXKFNj",
-    },
+        prem = "BCI6XW",
+    }, --6 char
 
     pivate_key = {
         beta = "b",
         prem = "p",
         free = "f",
-    },
+    }, --first letter
 }
