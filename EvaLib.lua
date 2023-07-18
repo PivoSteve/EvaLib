9fce4ccb861d8825123fe9732cb5bf73a3473f17
EvaCord <112832151+PivoSteve@users.noreply.github.com>
2023-07-18 20:47:38 +0600

    Update EvaLib.lua

--- EvaLib.lua
+++ EvaLib.lua
@@ -21,6 +21,11 @@ evalib = {
         free = "no date announced",
     }, --no date announced
 
+    free_stuff = {
+        text = "Buy premium to unlock this feature!",
+        text2 = "Buy premium to turn off advertisement!",
+    },
+
     last_update = {
         beta = "21.06.23",
         prem = "22.06.23",
@@ -30,7 +35,7 @@ evalib = {
     current_version = {
         beta = "10.5",
         prem = "10.5",
-        free = "8.1",
+        free = "8.5",
     }, --0.0
  
     key_check = {
